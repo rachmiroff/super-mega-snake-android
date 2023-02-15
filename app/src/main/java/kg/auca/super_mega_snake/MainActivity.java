b841caa8467ea3f729039dfef911321413b11ab4
Dmitrii Toksaitov <dmitrii@toksaitov.com>
2023-02-15 16:39:24 +0600

    Write the Main Activity basic nav logic (Closes #3)

--- app/src/main/java/kg/auca/super_mega_snake/MainActivity.java
+++ app/src/main/java/kg/auca/super_mega_snake/MainActivity.java
@@ -2,7 +2,9 @@ package kg.auca.super_mega_snake;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onSinglePlayerButtonClick(View view) {
+        Intent intent = new Intent(this, SinglePlayerGameActivity.class);
+        startActivity(intent);
+    }
+
+    public void onMultiPlayerButtonClick(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void onHighScoreButtonClick(View view) {
+        Intent intent = new Intent(this, HighScoreActivity.class);
+        startActivity(intent);
     }
 }
