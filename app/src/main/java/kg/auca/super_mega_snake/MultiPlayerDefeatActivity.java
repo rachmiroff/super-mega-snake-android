f429ca41e04d1af0d6dbae69970b5a3ef417a14e
Sarfaroz Yunusov <sarfarozyunusov@gmail.com>
2023-02-20 16:39:07 +0600

    Create the Multi-player Defeat Activity layout (Closes #17)

---
+++ app/src/main/java/kg/auca/super_mega_snake/MultiPlayerDefeatActivity.java
@@ -0,0 +1,14 @@
+package kg.auca.super_mega_snake;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MultiPlayerDefeatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multi_player_defeat);
+    }
+}
