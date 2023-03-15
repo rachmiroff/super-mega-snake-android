a27d9311f5da0c7e64bc141ec44fe7f7686997a9
gonzoden <deniz.nazarova.99@gmail.com>
2023-03-15 11:23:16 +0600

    Modify the Single-player Defeat Activity with New Record layout (Closes #14)

--- app/src/main/java/kg/auca/super_mega_snake/SinglePlayerDefeatActivityNewRecord.java
+++ app/src/main/java/kg/auca/super_mega_snake/SinglePlayerDefeatActivityNewRecord.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SinglePlayerDefeatActivity extends AppCompatActivity {
+public class SinglePlayerDefeatActivityNewRecord extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_single_player_defeat);
+        setContentView(R.layout.activity_single_player_defeat_new_record);
     }
 }
