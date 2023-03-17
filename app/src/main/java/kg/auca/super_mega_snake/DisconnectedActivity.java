f1d7f63d8df150dbbc0e5293fe0b8157edba473e
Akmatov Emirlan <crag5973@gmail.com>
2023-03-18 00:37:11 +0600

    Create the Disconnected Activity layout (Closes #9)

---
+++ app/src/main/java/kg/auca/super_mega_snake/DisconnectedActivity.java
@@ -0,0 +1,14 @@
+package kg.auca.super_mega_snake;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisconnectedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_disconnected);
+    }
+}
